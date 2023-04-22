df8a23a208ff9fbaad1cb6af2b5e3cb55198317b
Aung Thiha <aungthiha.160898@gmail.com>
2023-04-22 16:28:05 +0700

    added flutter dev configs

--- mappings.lua
+++ mappings.lua
@@ -11,13 +11,22 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr)
+          require("astronvim.utils.buffer").close(
+            bufnr)
+        end)
       end,
       desc = "Pick to close",
     },
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+    ["<leader>td"] = { "<cmd>TodoTelescope<cr>", desc = "Show Todos in project" },
+    ["<leader>FRUN"] = { "<cmd>FlutterRun<cr>", desc = "Run current flutter project" },
+    ["<leader>FR"] = { "<cmd>FlutterReload<cr>", desc = "Flutter hot reload" },
+    ["<leader>FD"] = { "<cmd>FlutterDevices<cr>", desc = "Get devices for Flutter" },
+    ["<leader>FSIM"] = { "<cmd>FlutterEmulators<cr>", desc = "Get sim/em devices for Flutter" },
+    ["<leader>Flr"] = { "<cmd>FlutterRename<cr>", desc = "Flutter rename and update imports" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
